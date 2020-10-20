39b0a367f1f957d6e64d520544ddecc2f554a7e6
Matias Di muro <matiasdimuro22@gmail.com>
2020-10-20 18:15:23 -0300

    termino app

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="text-center mb-3">
       <hr />
       <h4 className="text-muted">Hello React</h4>
-      <a href="https://github.com/santiagotrini/hello-react">
+      <a href="https://github.com/matiasdimuro/hello-react">
         <i className="fa fa-github fa-3x text-dark"></i>
       </a>
     </div>
